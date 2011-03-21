218920314edcc0fd7d87309bdaaf6fb94ab5f46c
Rio <rio517@gmail.com>
2011-03-21 02:36:45 -0400

    update gemspec

--- init.rb
+++ init.rb
@@ -2,10 +2,10 @@ require 'active_scaffold'
 
 
 #we can't copy assets on heroku
-unless ['production','staging'].include?(Rails.env)
-  #begin
+=begin
+  begin
     ActiveScaffoldAssets.copy_to_public(ActiveScaffold.root, {:clean_up_destination => true})
-  #rescue
-  #  raise $! unless ['production','staging'].include?(Rails.env)
-  #end
-end
+  rescue
+    raise $! unless ['production','staging'].include?(Rails.env)
+  end
+=end
